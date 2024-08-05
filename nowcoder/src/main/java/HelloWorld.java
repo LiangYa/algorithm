433a24dae2be47d1f8756c4cc7d0a330b6634ac3
liangya <liangya>
2024-08-05 11:54:14 +0800

    ADD：测试

--- nowcoder/src/main/java/HelloWorld.java
+++ nowcoder/src/main/java/HelloWorld.java
@@ -1,2 +1,7 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        String input = "Hello, World! This is a test...";
+        String output = input.replaceAll("[\\p{Punct}\\s]+", "");
+        System.out.println(output);
+    }
 }
